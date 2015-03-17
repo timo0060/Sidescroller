54bb34f10e592450c487776f2ee28fca113792e8
timo0060 <timo0060@gmail.com>
2015-03-17 14:55:37 -0400

    Added the preloader and basic code

--- SideScroller/Scripts/game.ts
+++ SideScroller/Scripts/game.ts
@@ -1,1 +1,36 @@
-﻿
+﻿//global variables
+var canvas;
+var stage: createjs.Stage;
+
+var assetLoader: createjs.LoadQueue;
+//Define the manifest with all the game assets
+var manifest = [
+    { id: "helicopter", src: "assets/images/helicopter.png" },
+    { id: "backgroundfirst", src: "assets/images/background-first.png" },
+    { id: "backgroundsecond", src: "assets/images/background-second.png"}
+];
+//This function is used to preload all of the assets
+function preload() {
+    assetLoader = new createjs.LoadQueue(); //instantiate the assetLoader
+    assetLoader.installPlugin(createjs.Sound);  //Install the sound plugin
+    assetLoader.on("complete", init, this); //When it's done loading, run the init function
+    assetLoader.loadManifest(manifest); //Load the manifest that's filled with the assets
+}
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+
+    main();
+}
+
+function gameLoop() {
+    stage.update();
+}
+
+function main() {
+    console.log("Game starting....");
+}
